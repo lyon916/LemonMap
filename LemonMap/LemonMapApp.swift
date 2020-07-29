f22df47810d6e266819c38c671b9d3dec9cd21b6
lyon <lixiaoyi@tonnn.com>
2020-07-29 10:03:03 +0800

    Initial Commit

---
+++ LemonMap/LemonMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  LemonMapApp.swift
+//  LemonMap
+//
+//  Created by Lyon on 2020/7/29.
+//
+
+import SwiftUI
+
+@main
+struct LemonMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
